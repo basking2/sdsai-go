18a848553baf02711bf753de341dc49bd9ec5f49
Sam Baskinger <basking2@yahoo.com>
2021-08-25 00:12:36 -0500

    Adding tests.

---
+++ integration/concurrentringcache_test.go
@@ -0,0 +1,26 @@
+package integration
+
+import (
+	"testing"
+	"time"
+
+	"github.com/basking2/sdsai-go/pkg/sdsai/cache"
+)
+
+func TestExpiration(t *testing.T) {
+	c := cache.NewConcurrentRingCache(10, 10, 1)
+
+	c.SetTimeFunction(func() int64 {
+		return time.Now().UnixNano()
+	})
+
+	c.Put("Hi", "Hi")
+	d, _ := time.ParseDuration("10ms")
+	time.Sleep(d)
+
+	if str, ok := c.Get("Hi"); ok {
+		t.Error("key found")
+	} else if str != "Hi" {
+		t.Error("expired key not returned")
+	}
+}
